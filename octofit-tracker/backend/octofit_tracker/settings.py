6fa1240ba01d939b0879824d25b6a7f5605eb2d6
KokilaPrabha <kokila.prabha@aspiresys.com>
2026-01-30 09:00:01 +0000

    Update settings and URLs to support Codespace environment configuration

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -26,8 +26,12 @@ SECRET_KEY = 'django-insecure-i8ely8+9ssq*-tsgm*6q1)j+=-5th$h6z(6$&g2drt&vo(n1-e
 DEBUG = True
 
 
-# Allow all hosts
-ALLOWED_HOSTS = ['*']
+# Allow localhost and Codespace URL
+import os
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if CODESPACE_NAME:
+    ALLOWED_HOSTS.append(f'{CODESPACE_NAME}-8000.app.github.dev')
 
 
 # Application definition
